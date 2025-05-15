45434d6f9dbed81f0cee2357a3fd3890d30f8d7f
BIN BIN <thangpd300502@gmail.com>
2025-05-15 18:34:11 +0700

    FE - AUTH PROVIDER

---
+++ frontend/src/providers/AuthProvider.tsx
@@ -0,0 +1,46 @@
+import { Loader } from "lucide-react";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "@/lib/axios";
+
+function updateAPIToken(token: string | null): void {
+	if (token) {
+		axiosInstance.defaults.headers.common[
+			"Authorization"
+		] = `Bearer ${token}`;
+	} else {
+		delete axiosInstance.defaults.headers.common["Authorization"];
+	}
+}
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+	const { getToken } = useAuth();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		async function initAuth() {
+			try {
+				const token = await getToken();
+				updateAPIToken(token);
+			} catch (error) {
+				console.log(error);
+				updateAPIToken(null);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+
+		initAuth();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div className="h-screen w-full flex items-center justify-center">
+				<Loader className="size-14 text-emerald-500 animate-spin" />
+			</div>
+		);
+	}
+	return children;
+};
+
+export default AuthProvider;
